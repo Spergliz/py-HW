65fbbe6dc68d056cc04e3591d33a30520ecf314e
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2023-01-09 14:31:15 -0700

    km

---
+++ calc.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter number ") )
+num2 = int(input("enter number "))
+
+total = num1 + num2
+
+print("total: " + str(total))
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+# print("hello world")
+name = input("what is your name")
+TOD = input("what is the time of day ")
+
+print(" hello " + name + "!")
+print("have a good " + TOD)
